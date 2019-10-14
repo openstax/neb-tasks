49688a09b65c8c9163c80ce7fc2ee1091948e5c9
tomjw64 <tom.jw64@gmail.com>
2019-10-14 16:06:53 -0500

    Add setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(req_file):
+    """Parse a requirements.txt file to a list of requirements"""
+    with open(req_file, 'r') as fb:
+        reqs = [
+            req for req in fb.readlines()
+            if req.strip() and not req.startswith('#')
+        ]
+    return list(reqs)
+
+install_requires = parse_requirements('requirements.txt')
+
+setup(
+    name='nebtasks',
+    author='Openstax',
+    author_email='info@cnx.org',
+    url="https://github.com/openstax/neb-tasks",
+    license='AGPL, See also LICENSE.txt',
+    description='Content task addons to neb',
+    install_requires=install_requires,
+    packages=find_packages(),
+    include_package_data=True
+)
