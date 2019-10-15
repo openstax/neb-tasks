f0121afcc9ea9525b33ee891f6a95a828352d086
tomjw64 <tom.jw64@gmail.com>
2019-10-15 16:12:22 -0500

    Add broken_imgs task

--- nebtasks/tasks/echo_task.py
+++ nebtasks/tasks/echo_task.py
@@ -1,9 +1,9 @@
 from nebu.cli.task_extensions import neb_task, NebTask
 
-@neb_task(name='echo_task')
+@neb_task(name='echo-task')
 def echo_task():
-    """echo back collection id"""
-    
+    """echo back collection/module ids"""
+
     def collection_action(id, file, resources):
         print(id)
 
