d37ec74729a0d40da9f0c12b0e42fe2847096eab
tomjw64 <tom.jw64@gmail.com>
2019-10-15 15:11:35 -0500

    test_task -> echo_task for example purposes

--- nebtasks/tasks/test_task.py
+++ nebtasks/tasks/test_task.py
@@ -1,5 +1,9 @@
 from nebu.cli.task_extensions import neb_task, NebTask
 
-@neb_task(name='test_task')
-def test_task():
-    return NebTask()
+@neb_task(name='echo_task')
+def echo_task():
+    """echo back collection id"""
+    def collection_action(id, file, resources):
+        print(id)
+
+    return NebTask(collection_action=collection_action)
