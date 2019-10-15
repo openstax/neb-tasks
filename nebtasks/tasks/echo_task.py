0bf65a11ba4ba1ccedcb2e92180cb80b742e8871
tomjw64 <tom.jw64@gmail.com>
2019-10-15 15:51:17 -0500

    test -> echo, add gitignore

--- nebtasks/tasks/echo_task.py
+++ nebtasks/tasks/echo_task.py
@@ -3,7 +3,13 @@ from nebu.cli.task_extensions import neb_task, NebTask
 @neb_task(name='echo_task')
 def echo_task():
     """echo back collection id"""
+    
     def collection_action(id, file, resources):
         print(id)
 
-    return NebTask(collection_action=collection_action)
+    def module_action(id, file, resources):
+        print(id)
+
+    return NebTask(
+        collection_action=collection_action,
+        module_action=module_action)
